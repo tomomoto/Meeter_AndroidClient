46be9e13b991fa9d4ba9a957a5ada9295f340230
Georgy Lebedik <venom369@yandex.ru>
2025-06-23 04:05:12 +0300

    Cleanup.

--- AndroidClient/src/main/java/com/tom/meeter/context/profile/user/domain/User.java
+++ AndroidClient/src/main/java/com/tom/meeter/context/profile/user/domain/User.java
@@ -4,11 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 @Entity
-@JsonIgnoreProperties(ignoreUnknown = true)
-//login?
 public class User {
 
     @PrimaryKey
@@ -19,10 +15,6 @@ public class User {
     private String surname;
     private String info;
     private String birthday;
-
-    public User() {
-        //Jackson.
-    }
 
     public User(
           @NonNull String id, String name, String gender, String surname,
